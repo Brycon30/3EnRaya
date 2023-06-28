79b9bb93a251fb9d2547eaad13b519acac7af7f9
tongggggo <107387050+tongggggo@users.noreply.github.com>
2023-06-28 16:14:41 -0700

    Update juanJaime.sql

--- juanJaime.sql
+++ juanJaime.sql
@@ -1,43 +1,48 @@
-create database conexionJavaSQL
+--SE CREA LA BASE DE DATOS
+create database tresenraya
 
-create table usuarios (
-idUsuario int IDENTITY NOT NULL PRIMARY KEY,
-nombreUsuario varchar(20) NOT NULL,
-contraseña varchar(20) NOT NULL, 
+--SE CREA LA TABLA DE USUARIOS/JUGADORES EN ESTE PUEDES CAMBIAR EL NOMBRE A LO QUE QUIERASXD
+create table jugadores (
+nombre varchar(50) NOT NULL primary key,
+contraseña varchar(50) NOT NULL, 
 telefono varchar(10) NOT NULL,
 partidasGanadas int NOT NULL,
 )
 
+--SE CREA LA TABLA PARTIDAS
 create table partidas (
 idPartida int IDENTITY NOT NULL PRIMARY KEY,
-usuarioO INT,
-usuarioX INT,
+jugadorO varchar(50),
+jugadorX varchar(50),
 usuarioGanador varchar(1),
-
-foreign key (usuarioO) references usuarios(idUsuario),
-foreign key (usuarioX) references usuarios(idUsuario)
+foreign key (jugadorO) references jugadores(nombre),
+foreign key (jugadorX) references jugadores(nombre)
 )
 
-alter trigger verificarUsuario
-ON usuarios
-FOR INSERT
+--SE CREA EL TRIGGER PARA VERIFICAR QUE EL USUARIO NO EXISTA
+create trigger verificarUsuario
+ON jugadores
+INSTEAD OF INSERT
 as
 begin
-
-declare @usuarioNombre varchar(20)
-select @usuarioNombre = nombreUsuario from inserted
-IF EXISTS (select*from usuarios where nombreUsuario = @usuarioNombre)
+IF EXISTS (select 1 from jugadores INNER JOIN inserted on jugadores.nombre = inserted.nombre)
 begin
 	RAISERROR('El usuario ya existe', 16,1)
 	ROLLBACK TRANSACTION;
 end
+ELSE
+BEGIN
+INSERT INTO jugadores (nombre,contraseña,telefono,partidasGanadas) select nombre, contraseña, telefono, partidasGanadas from inserted
+END
 
 end
 
-insert into usuarios values('tongoxd','hailgrasa','6971133066',10)
-insert into usuarios values('bry555','enchiladas','6971094100',4)
-insert into usuarios values('bry555','enchildas2','6971104201',5)
-
-delete from usuarios where contraseña = 'enchildas2'
-
-select*from usuarios
+--SE CREA EL PROCEDIMIENTO ALMACENADO QUE AUMENTA LAS PARTIDAS GANADAS Y SE LE ENVÍA DE VALOR EL NOMBRE JUGADOR GANADOR
+create procedure ganarPartida
+@nombre varchar(20)
+as
+begin
+update jugadores
+set partidasGanadas = partidasGanadas+1
+where nombre = @nombre
+end
